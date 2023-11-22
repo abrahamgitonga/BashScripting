96cfa6311283db586beed59c0dc22c2921678c9b
abraham <abrahamgitonga98@gmail.com>
2023-11-22 21:39:36 +0300

    update#3

--- error_handling.sh
+++ error_handling.sh
@@ -2,6 +2,8 @@
 
 readonly input="$1"
 
+#This block checks whether the number of command-line arguments ($#) is not equal to 1.
+
 if [ $# -ne 1 ]
 then
     echo "Usage: $(basename -a $0) <person>"
