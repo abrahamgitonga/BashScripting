609c6547154ec992bc3ff3a26a5659eeb5b380ab
abraham <abrahamgitonga98@gmail.com>
2023-11-26 22:26:56 +0300

    #WIFIsate x Brightness level check#262226

--- date.sh
+++ date.sh
@@ -5,3 +5,15 @@ echo Hellow $1,Today is $(date +"%A, %B %d, %Y %H:%M:%S" )
 #uname -a command retrieves your kernel info
 kernel_details=$(uname -a)
 echo "You are using the following kernel: $kernel_details"
+
+# Check WiFi state using nmcli
+wifi_state=$(nmcli -t -f WIFI general)
+if [ "$wifi_state" == "enabled" ]; then
+    echo "WiFi is enabled."
+else
+    echo "WiFi is disabled."
+fi
+
+# Check screen brightness level using xrandr
+brightness_level=$(xrandr --verbose | grep -i brightness | awk '{print $2}')
+echo "Screen brightness level: $brightness_level"
